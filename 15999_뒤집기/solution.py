50c62c81f56834ba5fdd48cfd9a1e96c0b620228
jack9410 <jack941028@naver.com>
2020-11-11 00:42:29 +0900

    Update solution.py

--- 15999_뒤집기/solution.py
+++ 15999_뒤집기/solution.py
@@ -1,5 +1,8 @@
 # 참고:https://thyong.tistory.com/entry/%EB%B0%B1%EC%A4%80BOJ-15999%EB%B2%88-%EB%92%A4%EC%A7%91%EA%B8%B0-Python3
 
+# 처음에는 직접 판을 다 눌러보면서 풀었지만 논리적으로 접근해봐야 하는 문제
+# 눌러도 최종 형태에 영향을 주지 않는 칸을 찾아야한다.
+
 # 입력 받기
 N, M = map(int, input().split())
 grid = [[0 for _ in range(M)] for _ in range(N)]
