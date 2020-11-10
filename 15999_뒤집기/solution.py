d753f6c9f6972dcab87290da95726f1f8229d9a4
jack9410 <jack941028@naver.com>
2020-11-11 00:41:27 +0900

    Create solution.py

---
+++ 15999_뒤집기/solution.py
@@ -0,0 +1,28 @@
+# 참고:https://thyong.tistory.com/entry/%EB%B0%B1%EC%A4%80BOJ-15999%EB%B2%88-%EB%92%A4%EC%A7%91%EA%B8%B0-Python3
+
+# 입력 받기
+N, M = map(int, input().split())
+grid = [[0 for _ in range(M)] for _ in range(N)]
+
+for n in range(N):
+    grid[n] = list(input())
+
+dx = [0,0,1,-1]
+dy = [1,-1,0,0]
+
+count = 0
+answer = 1
+
+for y in range(N):
+    for x in range(M):
+        for i in range(4):
+            nx = x + dx[i]
+            ny = y + dy[i]
+
+            if 0<=ny<N and 0<=nx<M:
+                if grid[y][x] != grid[ny][nx]:
+                    count+=1
+                    break
+
+answer = pow(2, N*M-count) % 1000000007
+print(answer)
