f41180273a3a4627ef04e69bb77250bf481185e4
jack9410 <46255505+jack9410@users.noreply.github.com>
2021-01-18 21:20:10 +0900

    안전영역 풀이

--- 2468_안전영역/solution.py
+++ 2468_안전영역/solution.py
@@ -3,30 +3,51 @@ import sys
 curr_dir = os.path.dirname(__file__)
 sys.stdin = open(curr_dir + '\input.txt', 'r')
 
+from collections import deque
+
 N = int(input())
-max_height = 0
 land = [list(map(int, input().split())) for _ in range(N) ]
+
+# print(land)
 
 dx = [-1,0,1,0]
 dy = [0,1,0,-1]
 
-visited = [[True]*N for _ in range(N)]
-print(visited)
+def bfs(x, y):
+    q = deque()
+    q.append([x, y])
+    visited[y][x] = True
 
-def inBound(x,y):
-    if 0<=x<N and 0<=y<N:
-            return True
-    else:
-        return False
+    while q:
+        x, y = q.popleft()
+        for i in range(4):
+            nx = x + dx[i]
+            ny = y + dy[i]
 
-def find(x, y rain):
-    for i in range(4):
-        nx = x + dx[i]
-        ny = y + dy[i]
+            if 0 <= nx < N and 0 <=ny < N:
+                if not visited[ny][nx]:
+                    visited[ny][nx] = True
+                    q.append([nx, ny])
+                    
 
-        if inBound(nx, ny):
-            if visited
-
+tmp_lst = []
 
 for rain in range(100):
+    visited = [[False]*N for _ in range(N)]
+    n = 0
 
+    for y in range(N):
+        for x in range(N):
+            if land[y][x] <= rain:
+                visited[y][x] = True
+
+    for y in range(N):
+        for x in range(N):
+            if not visited[y][x]:
+                bfs(x,y)
+                n += 1
+
+    tmp_lst.append(n)
+
+# print(tmp_lst)
+print(max(tmp_lst))
