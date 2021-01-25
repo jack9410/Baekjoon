72f26c92d4191d83a2c08a9eb45938ddf9264dfe
jack9410 <jack941028@naver.com>
2021-01-25 22:45:46 +0900

    계단오르기 수정

--- 2579_계단_오르기/solution.py
+++ 2579_계단_오르기/solution.py
@@ -1,4 +1,5 @@
 # 틀림
+# 1 일때 생각하자
 
 import os
 import sys
@@ -6,21 +7,21 @@ curr_dir = os.path.dirname(__file__)
 sys.stdin = open(curr_dir + '\input.txt', 'r')
 
 N = int(input())
-arr = []
-dp = []
+arr = [0 for _ in range(301)]
+dp = [0 for _ in range(301)]
 
-for _ in range(N):
-    arr.append(int(input()))
+for i in range(N):
+    arr[i] = int(input())
 
-dp.append(arr[0])
-dp.append(arr[0]+ arr[1])
-dp.append(max(arr[0]+arr[2], arr[1]+arr[2]))
+dp[0] = arr[0]
+dp[1] = arr[0]+ arr[1]
+dp[2] = max(arr[0]+arr[2], arr[1]+arr[2])
 
 for i in range(3, N):
-    dp.append(max(dp[i-3] + arr[i-1] + arr[i], dp[i-1] + arr[i]))
+    dp[i] = max(dp[i-3] + arr[i-1] + arr[i], dp[i-2] + arr[i])
 
-print(dp)
-print(dp[-1])
+# print(dp)
+print(dp[N-1])
 
 
 
